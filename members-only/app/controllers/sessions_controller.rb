8e29f71aa27a355f2cf69705218d6b105cec98f2
Ashah15 <ashaabdillahi53@gmail.com>
2019-11-28 00:25:18 +0300

    application,js, sessions_controller.rb, sessions_helper.rb, _header.html.erb

--- members-only/app/controllers/sessions_controller.rb
+++ members-only/app/controllers/sessions_controller.rb
@@ -15,4 +15,10 @@ class SessionsController < ApplicationController
 
   end
 
+  def destroy
+
+	log_out if logged_in?
+	redirect_to root_url
+  end
+
 end
--- members-only/app/helpers/sessions_helper.rb
+++ members-only/app/helpers/sessions_helper.rb
@@ -4,8 +4,12 @@ module SessionsHelper
 	end 
 
 	def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
+    	if session[:user_id]
+      		@current_user ||= User.find_by(id: session[:user_id])
+    	end
+  	end
+
+  	def logged_in?
+    	!current_user.nil?
+  	end
 end
