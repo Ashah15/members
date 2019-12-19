9f13ee411cd006c725a2203afd64d4e7a528dc45
Marvellous Michaels <marvellousubani@gmail.com>
2019-12-19 17:34:34 +0100

    fixed sign up controller issues

--- members-only/app/controllers/users_controller.rb
+++ members-only/app/controllers/users_controller.rb
@@ -1,35 +1,32 @@
 class UsersController < ApplicationController
 	def new
-		@user = User.new
-		
+		@user = User.new		
 	end
 
 	def create
-
-  		@user = User.new(user_params)
-  			if @user.save
-  			
-      			redirect_to root_url
-  			else
-  			render 'new'
-  			end
-		
+	  @user = User.new(user_params)
+		if @user.save
+      log_in @user
+		  redirect_to root_url
+		else
+		 render 'new'
+		end
 	end
 
 	def show
-  		@user = User.find(params[:id])
+    @user = User.find(params[:id])
 	end	
 
 	private
 	  	def user_params
-    		params.require(:user).permit(:name, :email)
-      	end
+    		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 
     	def logged_in_user
-      		unless logged_in?
-        	store_location
-        	flash[:danger] = "Please log in."
-        	redirect_to login_url
-        end
+      	unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+	root 'posts#index'
   get  '/signup',  to: 'users#new'
 	get '/login', to: 'sessions#new'
 	post 'login', to: 'sessions#create'
